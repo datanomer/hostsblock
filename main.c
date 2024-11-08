741194d3a7cc26b095b4f9cec5850d3d2e9d7cda
datanomer <niccsacc@protonmail.com>
2024-11-09 00:16:14 +0200

    added halfworking blocklist feature

--- main.c
+++ main.c
@@ -21,7 +21,10 @@
 #define HFILE "/etc/hosts"
 #define LOCALH "localhost"
 #define LOCALIP6 "::1"
+#define IBUF 128
+
 FILE * filep;
+FILE * tempfp;
 
 bool block_exists(char *input_buf)
 {
@@ -76,6 +79,7 @@ void block_add(char *input_buf)
         time(&timestamp);
         fprintf(filep,"\n" LOCALH "       %s #(Added by hostsblock) Timestamp: %s ", input_buf, ctime(&timestamp));
         fprintf(filep,""LOCALIP6 "             %s ", input_buf);
+        
         printf("[*] Blocking %s ...\n", input_buf);
         
         fclose(filep);
@@ -118,7 +122,7 @@ void block_del(char *input_buf)
         }
 
         filep = fopen(HFILE, "r");
-        tempfp = fopen(".hbtemp.txt", "w");    
+        tempfp = fopen(".hbtemp", "w");    
            
         while ((fgets(buffer, 256, filep)) != NULL) {
             if ((strstr(buffer, input_buf)) == NULL) {
@@ -134,15 +138,15 @@ void block_del(char *input_buf)
             }
         line_num++;
         }
-                    
+    
         printf("\n[*] %s unblocked ... \n ", input_buf);
         
-        rename(".hbtemp.txt", "hosts"); 
+        rename(".hbtemp", "hosts"); 
         system("mv hosts "HFILE"");
         //system("cat testetc/hosts");
-        remove(".hbtemp.txt");
+        remove(".hbtemp");
         remove("hosts");
-        system("rm -rf hosts");
+    system("rm -rf hosts");
         fclose(filep);
         fclose(tempfp);
         printf("---------|/etc/hosts|----------\n");
@@ -159,28 +163,65 @@ void block_del(char *input_buf)
         }
     }
 }
+void blocklist_add(char *input_list)
+{
+    tempfp = fopen(input_list, "r");
+    filep = fopen(HFILE, "a+");
+    
+    int counter = 0;
+    int line_num = 1;
+    // open stream 
+    char domain_buf[256];
+    
+    //printf("[*] Blocking %s \n", domain_buf);
+    fflush(filep);
+    fflush(tempfp);
+    fprintf(filep,"\n# Added by hostsblock -->");
+    while(fgets(domain_buf, 256, tempfp) != NULL){
+        //fputs(domain_buf, filep);
+        fprintf(filep,"\n" LOCALH "         %s ", domain_buf);
+        fprintf(filep,""LOCALIP6 "             %s ", domain_buf);
+    }
+    fprintf(filep, "# <-- Added by hostsblock");
+    fclose(tempfp);
+    fclose(filep);
+}
 
 int main(int argc, char** argv)
 {
-
+         
     //check if access to file
     struct stat statbuf;
     int fildes = open(HFILE, O_RDWR);
-    int status = fstat(fildes, &statbuf);
-
-    if (status == -1) {
+    int fstatus = fstat(fildes, &statbuf);
+    
+    if (fstatus == -1) {
         printf("[!] You have to run program as superuser!\n");
         exit(-1);
     }
 
-    char input_buf[128];
-    // take input from a external blocklist
+    char *input_list = argv[1];
+    // TD: take input from a external blocklist
     // on error case output help msg.
+    FILE *tempfp;
 
-    if (argc > 3) {
-        printf("[!] Too many args.\n");
-        printf("Usage: hblock <arg> <file>"); 
-        return -1;
+    if (argc == 2) {
+        fildes = open(argv[1], O_RDWR);
+        fstatus = fstat(fildes, &statbuf);
+        if (fstatus == -1) {
+            printf("[!] File not found.\n");    
+            printf("Usage: hblock <blocklist.txt>"); 
+            exit(-1);
+
+        }
+
+        printf("[*] Using file input: %s \n", argv[1]);
+        blocklist_add(input_list);        
+        exit(0); 
+    }
+    else if (argc == 1) {
+    }
+    else {
     }
 
     printf("|--hostsblock--|\n");
@@ -190,11 +231,14 @@ int main(int argc, char** argv)
     printf("\n--------------EOF--------------\n");
     printf("Choice: ");
     int in;
+    char input_buf[IBUF];
+    //fgets(input_buf, IBUF, stdin);
     scanf("%d", &in);
-
+    
     if (in == 1) {
         printf("Type the site url (ex. twitter.com): ");  
         scanf("%s", input_buf);
+        //fgets(input_buf, IBUF, stdin); doenst work
         block_add(input_buf);
 
     }
